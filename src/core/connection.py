f10cbe776e83713f6c70a1bab489d58af027405d
Nathan Burns <nburns9922@gmail.com>
2024-12-17 19:01:07 -0800

    add ssh connection handlers and skeleton functions

--- src/core/connection.py
+++ src/core/connection.py
@@ -1,10 +1,12 @@
 # SOAP API and SSH connection management
+import paramiko.client
 from core.authenticator import ESXiAuthenticator
 import logging
 import requests
 import xmltodict
+import paramiko
 from dataclasses import dataclass
-
+import time
 # logger boilerplate
 logger = logging.getLogger(__name__)
 
@@ -20,11 +22,13 @@ class ESXiConnection:
         :param verify_ssl: SSL Certification verification
         """
         self.username = username
+        self.password = password
         self.verify_ssh=verify_ssl
         self.authenticator = ESXiAuthenticator(host, username=username, password=password, verify_ssl=verify_ssl)
         self.base_url = f"https://{host}/sdk/"
         self.host = host
         self.headers = None
+        self.ssh_conn = None
         
     def connect_api(self):
         """
@@ -44,7 +48,33 @@ class ESXiConnection:
         
         :return: SSH session
         """
-        raise NotImplementedError
+        #TODO: ERROR HANDLING WHEN SSH CONNECTION CANNOT BE MADE
+        client = paramiko.client.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(self.host, username=self.username, password=self.password)
+        self.ssh_conn = client
+        
+    def send_ssh_command(self, command: str):
+        """
+        Sends an SSH command to the ESXi host
+        
+        :param: command (str): Command to send
+        :return: True if successful, False if unsuccessful
+        """
+        #_stdin, _stdout, _stderr = self.ssh_conn.exec_command(command)
+        #print(_stdout.read().decode())
+        # need to send command via `invoke_shell` for it to show up in `shell.log` in ESXi system
+        shell = self.ssh_conn.invoke_shell()
+        # not sure why we need so many sleeps??
+        # TODO: ERROR HANDLING WHEN SSH COMMAND FAILS
+        time.sleep(1)
+        shell.send('echo "execute command with esxi-testing-toolkit"\r\n')
+        time.sleep(1)
+        shell.send('ls -la\r\n')
+        time.sleep(1)
+        data = shell.recv(2048)
+        print(data.decode())
+        
     def send_request(self, payload):
         """
         Sends provided payload to ESXi host
@@ -75,6 +105,19 @@ class ESXiConnection:
             raise SystemExit()
         except Exception as e:
             logging.error(f'Failed to send payload: {e}')
+    def return_logs(self, log_file: str, lines: int):
+        """
+        Returns lines from specific log file to console.
+        
+        :param: log_file (str): Log file to get lines from.
+        :param: lines (int): Number of lines to return.
+        """
+        logging.error('Returning log from ESXi host is not yet supported!')
+        
+        # need to use generic SSH connection to pull logs back to stdout
+        # ssh_conn = self.connect_ssh()
+        # ssh_conn.execute_ssh('cat /var/log/hostd.log')
+        raise NotImplementedError
             
 def parse_request_error(request_response: str):
     """
