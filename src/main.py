e651550e6a6e1f02e4cf999df1268dc14e3a9888
Nathan Burns <nburns9922@gmail.com>
2024-12-30 13:41:30 -0800

    split logging format

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@
 import typer
 import requests
 import logging
-
 # import core components
 from core.connection import ESXiConnection
 from core.config_manager import retrieve_secrets
@@ -10,17 +9,20 @@ import core.config_manager
 import cli.vm_commands
 # used to suppress insecure request warnings from requests
 from urllib3.exceptions import InsecureRequestWarning
-
 # suppress insecure request warning for self-signed SSL cert in ESXi hosts.
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+
 # logging boilerplate
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO,format='%(asctime)s | %(levelname)-5s | %(message)s')
+
+FORMAT = '%(asctime)s | %(levelname)-5s | %(message)s'
+
+logging.basicConfig(level=logging.INFO,format=FORMAT)
 
 # typer boilerplate
 app = typer.Typer()
-
+# add commands from cli/vm_commands into the app
 app.add_typer(cli.vm_commands.app, name="vm", help="Perform actions on Virtual Machines: delete_vm_snapshots | power_off_vm")
 
 # app entrypoint
