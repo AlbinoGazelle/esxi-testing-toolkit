fbcf2720478f27030e38461c6f38134afe7da436
Nathan Burns <nburns9922@gmail.com>
2025-01-06 21:24:30 -0800

    update tests to use new command structure

--- tests.sh
+++ tests.sh
@@ -7,10 +7,20 @@ pipx uninstall-all
 pipx install .
 
 # execute tests
-esxi-testing-toolkit vm delete-vm-snapshots 1 api --verbose
+esxi-testing-toolkit vm delete-vm-snapshots --vm-id=1 --method=api --verbose
 
-esxi-testing-toolkit vm delete-vm-snapshots 1 api --no-verbose
+esxi-testing-toolkit vm delete-vm-snapshots --vm-id=1 --method=ssh --verbose
 
-esxi-testing-toolkit vm delete-vm-snapshots 1 ssh --verbose
+esxi-testing-toolkit vm power-off-vm --vm-id=1 --method=api --verbose
 
-esxi-testing-toolkit vm delete-vm-snapshots 1 ssh --no-verbose
+esxi-testing-toolkit vm power-off-vm --vm-id=1 --method=ssh --verbose
+
+esxi-testing-toolkit host disable-autostart --method=ssh --verbose
+
+esxi-testing-toolkit host disable-autostart --method=api --verbose
+
+esxi-testing-toolkit host enable-ssh --method=ssh --verbose
+
+esxi-testing-toolkit host enable-ssh --method=api --verbose
+
+esxi-testing-toolkit host get-all-vm-ids --method=api --verbose
