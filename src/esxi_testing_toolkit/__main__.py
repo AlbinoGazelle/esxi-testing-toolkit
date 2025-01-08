fdb7410ae372414d1a549bc27454b3993b120911
Nathan Burns <nburns9922@gmail.com>
2025-01-08 14:42:00 -0800

    fix imports

--- src/esxi_testing_toolkit/__main__.py
+++ src/esxi_testing_toolkit/__main__.py
@@ -3,9 +3,9 @@ import typer
 import requests
 import logging
 # import core components
-import cli.base_commands
-import cli.host_commands
-import cli.vm_commands
+import esxi_testing_toolkit.cli.vm_commands
+import esxi_testing_toolkit.cli.host_commands
+import esxi_testing_toolkit.cli.base_commands
 # used to suppress insecure request warnings from requests
 from urllib3.exceptions import InsecureRequestWarning
 # suppress insecure request warning for self-signed SSL cert in ESXi hosts.
@@ -23,9 +23,9 @@ logging.basicConfig(level=logging.INFO,format=FORMAT)
 app = typer.Typer()
 
 # add commands from cli/vm_commands into the app
-app.add_typer(cli.vm_commands.app, name="vm", help="Perform actions on Virtual Machines: delete_vm_snapshots | power_off_vm")
-app.add_typer(cli.host_commands.app, name="host", help="Performs actions on the ESXi host.")
-app.add_typer(cli.base_commands.app, name="base", help="Display information about available tests")
+app.add_typer(esxi_testing_toolkit.cli.vm_commands.app, name="vm", help="Perform actions on Virtual Machines: delete_vm_snapshots | power_off_vm")
+app.add_typer(esxi_testing_toolkit.cli.host_commands.app, name="host", help="Performs actions on the ESXi host.")
+app.add_typer(esxi_testing_toolkit.cli.base_commands.app, name="base", help="Display information about available tests")
 
 # app entrypoint
 def main():
